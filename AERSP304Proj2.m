22350f1f65ecab6e4408077092b10bfe8b28fe4e
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-03-23 13:18:53 -0400

    added constants

---
+++ AERSP304Proj2.m
@@ -0,0 +1,13 @@
+% QUAD-ROTOR AERSP 304
+% By : Nicholas Giampetro, Craig Stenstrom, Payton Glynn
+
+% const values
+g = 9.81 ; % m/s
+m = 0.450 ; % kg
+l = 0.225 ; % 
+k = 2.98*10^-7 ;
+b = 1.14*10^-6 ;
+I = [ 4.85*10^-3 , 0, 0 ;
+     0 , 4.85*10^-3 , 0 ;
+     0 , 0 , 8.80*10^-3 ] ;
+
